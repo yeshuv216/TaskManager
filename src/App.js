36aaf1d0d6f946dfc5546971d1168a0e1934ed79
Maykkkk <guptamayank1206@gmail.com>
2023-11-29 14:14:48 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import TaskList from './components/TaskList';
+import AddTaskForm from './components/AddTaskForm';
+import EditTaskForm from './components/EditTaskForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<TaskList />} />
+        <Route path="/add" element={<AddTaskForm />} />
+        <Route path="/edit/:taskId" element={<EditTaskForm />} />
+      </Routes>
+    </Router>
   );
 }
 
